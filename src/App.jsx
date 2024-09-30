cf057e78321801d387c8123b48d5548cda6ec221
daotienluc <lucd1177@gmail.com>
2024-09-30 21:16:28 +0700

    làm xong bài tập

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Banner from "./components/BaiTapThucHanhLayout/Banner";
+import Content from "./components/BaiTapThucHanhLayout/Content";
+import Footer from "./components/BaiTapThucHanhLayout/Footer";
+import Header from "./components/BaiTapThucHanhLayout/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <Content />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
